5d630ca28219486cace718475fe0d69e5a1b8e47
Federico Ragona <federico.ragona@gmail.com>
2021-05-29 16:50:39 +0200

    Reorganise code

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"github.com/fedragon/go-dedup/internal/db"
-	"github.com/fedragon/go-dedup/internal/fs"
 	"github.com/fedragon/go-dedup/internal/metrics"
+	"github.com/fedragon/go-dedup/pkg"
 	"log"
 	"os"
-	"runtime"
-	"sync"
 )
 
 func main() {
@@ -32,48 +30,5 @@ func main() {
 		}
 	}()
 
-	media := fs.Walk(mx, os.Getenv("ROOT"))
-
-	numWorkers := runtime.NumCPU()
-	workers := make([]<-chan int64, numWorkers)
-	for i := 0; i < numWorkers; i++ {
-		workers[i] = db.Store(mx, dbase, media)
-	}
-
-	done := make(chan struct{})
-	defer close(done)
-
-	var upserted int64
-	for i := range merge(done, workers...) {
-		if upserted > 0 && upserted%1000 == 0 {
-			log.Printf("upserted %v rows so far\n", upserted)
-		}
-		upserted += i
-	}
-}
-
-func merge(done <-chan struct{}, channels ...<-chan int64) <-chan int64 {
-	var wg sync.WaitGroup
-
-	wg.Add(len(channels))
-	media := make(chan int64)
-	multiplex := func(c <-chan int64) {
-		defer wg.Done()
-		for i := range c {
-			select {
-			case <-done:
-
-				return
-			case media <- i:
-			}
-		}
-	}
-	for _, c := range channels {
-		go multiplex(c)
-	}
-	go func() {
-		wg.Wait()
-		close(media)
-	}()
-	return media
+	pkg.Index(mx, dbase, os.Getenv("ROOT"))
 }
